8a2ce7d2d2fdbd60bd0aada2a75560eb7f029e8d
AlissaSquared <vandor2012@gmail.com>
2014-07-28 16:50:40 -0500

    I'm sorry about all those "page build warnings" y'all's gettin'. Fixin' now.

--- smoothlinks.js
+++ smoothlinks.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 			 if ($target) {
 				var targetOffset = $target.offset().top;
 				$(this).click(function() {
-					$('html, body').animate({scrollTop: targetOffset}, 400);
+					$('html, body').animate({scrollTop: targetOffset}, 1000);
 					return false;
 				});
 			}
